b99aeff20b258bf29823dd0047704fdb20e1ce1e
Andrew Mukare <dremukare@gmail.com>
2021-08-11 23:22:26 +0300

    Update amenity.py
    fixed error

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
-import os
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """represents the amenities in the place"""
-    __tablename__ = 'amenities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    """ represents Amentiy"""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place", backref="amenities", cascade="all, delete")
+        # place_amenities = relationship("Place", secondary=place_amenity)
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """intializes city"""
+        """initalize amentie"""
         super().__init__(*args, **kwargs)
